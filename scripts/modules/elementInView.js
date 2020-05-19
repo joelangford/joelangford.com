2fb11ab7eb66de97ce0e85527d1b79277ca266f1
Joe Langford <dev@joelangford.com>
2020-05-19 17:15:56 +0100

    optimising routing

--- scripts/modules/elementInView.js
+++ scripts/modules/elementInView.js
@@ -1,14 +1,15 @@
-const elementInView = () => {
+export const checkElements = () => {
   const elements = document.querySelectorAll('.js-fade-in');
-  const $body = document.querySelector('body');
 
-  const checkElements = () => {
-    elements.forEach($element => {
-       if ($element.getBoundingClientRect().top - document.documentElement.clientHeight < 0) {
-         $element.classList.add('animate');
-       }
-    });
-  };
+  elements.forEach($element => {
+     if ($element.getBoundingClientRect().top - document.documentElement.clientHeight < 0) {
+       $element.classList.add('animate');
+     }
+  });
+};
+
+const elementInView = () => {
+  const $body = document.querySelector('body');
 
   const init = () => {
     $body.classList.add('animations-active');
--- scripts/modules/routing.js
+++ scripts/modules/routing.js
@@ -1,5 +1,6 @@
-const routing = () => {
+import checkElements from './elementInView.js';
 
+const routing = () => {
   const requestHTML = (url) => {
     return new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
@@ -9,7 +10,7 @@ const routing = () => {
           resolve(xhr.responseXML);
         } else {
           reject({
-            status: this.status,
+            status: xhr.status,
             statusText: xhr.statusText
           });
         }
@@ -21,25 +22,121 @@ const routing = () => {
     });
   }
 
-  const init = () => {
-    const navLinks = document.querySelectorAll('.js-nav-link');
+  const parseContentHTML = (xml) => {
+    const content  = xml.querySelector('.js-page-content');
 
+    if (content) {
+      return content;
+    }
+  }
+
+  const hideAllChildren = ($parent) => {
+    const children = $parent.querySelectorAll('.js-page-content');
+
+    children.forEach($child => {
+      $child.style.display = 'none';
+    });
+  }
+
+  const injectContentHtml = (content) => {
+    const $contentContainer = document.querySelector('#content');
+
+    const contentCached = $contentContainer.querySelector(`#${content.getAttribute('id')}`);
+    hideAllChildren($contentContainer);
+
+    if (contentCached) {
+      contentCached.style.display = 'block';
+    } else {
+      $contentContainer.appendChild(content);
+      checkElements();
+    }
+  }
+
+  const updateNavigation = (navLinks) => {
+    const contentAreas = document.querySelectorAll('.js-page-content');
+    navLinks.forEach($navLink => {
+      contentAreas.forEach($contentArea => {
+        if ($contentArea.style.display !== 'none') {
+          if ($contentArea.getAttribute('id') === $navLink.getAttribute('data-content-target')) {
+            $navLink.classList.add('active');
+          } else {
+            $navLink.classList.remove('active');
+          }
+        }
+      });
+    });
+  }
+
+  const updateURL = (targetUrl) => {
+    history.pushState(null, null, targetUrl);
+  }
+
+  const removeFirstLoad = () => {
+    const elements = document.querySelectorAll('.first-load');
+
+    elements.forEach($element => {
+      $element.classList.remove('first-load');
+    });
+  }
+
+  const requestContent = (targetUrl, $targetLink, navLinks) => {
+    removeFirstLoad();
+    let $cachedContent;
+
+    if ($targetLink) {
+      updateURL(targetUrl);
+      $cachedContent = document.querySelector(`#${$targetLink.getAttribute('data-content-target')}`);
+    }
+
+    if ($cachedContent) {
+      const $contentContainer = document.querySelector('#content');
+
+      hideAllChildren($contentContainer);
+
+      $cachedContent.style.display = 'block';
+      updateNavigation(navLinks);
+    } else {
+      requestHTML(targetUrl)
+        .then((result) => {
+          return parseContentHTML(result);
+        }).then((result) => {
+          injectContentHtml(result);
+          updateNavigation(navLinks);
+        })
+        // .catch((err) => {
+        //   console.error('Error', err.statusText);
+        // });
+    }
+
+  }
+
+  const handleNavLinks = (navLinks) => {
     navLinks.forEach($navLink => {
       $navLink.addEventListener('click', (e) => {
         e.preventDefault();
 
-        const targetUrl = e.target.getAttribute('href');
+        const $targetLink = e.target;
+        const targetUrl = $targetLink.getAttribute('href');
 
-        requestHTML(targetUrl)
-          .then((result) => {
-            console.log(result);
-
-          })
-          .catch((err) => {
-            console.error('Error', err.statusText);
-          });
+        requestContent(targetUrl, $targetLink, navLinks);
       });
     });
+  }
+
+  const hanndlePopState = (navLinks) => {
+    window.addEventListener('popstate', (e) => {
+      const $targetLink = null;
+      const targetUrl = e.target.location.href;
+
+      requestContent(targetUrl, null, navLinks);
+    });
+  }
+
+  const init = () => {
+    const navLinks = document.querySelectorAll('.js-nav-link');
+
+    handleNavLinks(navLinks);
+    hanndlePopState(navLinks);
   }
 
   init();
